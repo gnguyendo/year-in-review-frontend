421606f5a371e42cb776225450f5d73cb9cf14b0
geoffrey <gnguyendo@gmail.com>
2023-02-01 18:02:34 -0800

    removed and added basic html for testing

---
+++ components/Navbar.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+
+const Navbar = () => {
+    return (
+        <nav>
+            <div className="nav">
+                <Link href="/">Home</Link>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
